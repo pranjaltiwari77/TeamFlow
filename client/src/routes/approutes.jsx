8197762ce890234e8a3369d92e781a7c25ee4dde
pranjal tiwari <pranjaltiwari@pranjals-MacBook-Air.local>
2026-07-18 20:37:02 +0530

    Fix Navigate import

--- client/src/routes/approutes.jsx
+++ client/src/routes/approutes.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Navigate,
+  } from "react-router-dom";
 
 import Login from "../pages/login";
 import Register from "../pages/register";
